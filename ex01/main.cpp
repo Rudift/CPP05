76475a5d89379532b15016d0f78ba48cc79f9d78
Valentin Deliere <vdeliere@f6r8s7.clusters.42paris.fr>
2025-09-22 15:56:05 +0200

    ex01 fini

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -15,7 +15,7 @@
 
 int main(){
 
-	std::cout << YELLOW + "\n/------Test 1 : creation of form with too high grade requirement ------/" + RESET << std::endl;
+	std::cout << YELLOW + "\n/------Test 1 : creation of a form with too high grade requirement ------/" + RESET << std::endl;
 	try{
 		Form	ip53("ip53", -50, 3);
 	}
@@ -23,7 +23,7 @@ int main(){
 		std::cerr << RED + "Test 1 failed: " << e.what() << RESET << std::endl;
 	}
 
-	std::cout << YELLOW + "\n/------Test 2 : creation of form with too low grade requirement ------/" + RESET << std::endl;
+	std::cout << YELLOW + "\n/------Test 2 : creation of a form with too low grade requirement ------/" + RESET << std::endl;
 	try{
 		Form	ip53("ip53", 153, 3);
 	}
@@ -31,4 +31,23 @@ int main(){
 		std::cerr << RED + "Test 2 failed: " << e.what() << RESET << std::endl;
 	}
 
+	std::cout << YELLOW + "\n/------Test 3 : a too low bureaucrat try to sign the form ------/" + RESET << std::endl;
+	try{
+		Form		ip53("ip53", 50, 50);
+		Bureaucrat	bob("Bob", 53);	
+		bob.signForm(ip53);
+	}
+	catch (std::exception &e){
+		std::cerr << RED + "Test 3 failed: " << e.what() << RESET << std::endl;
+	}
+
+	std::cout << YELLOW + "\n/------Test 4 : the bureaucrat succeed to sign the form------/" + RESET << std::endl;
+	try{
+		Form		b43("b43", 50, 50);
+		Bureaucrat	terry("Terry", 46);	
+		terry.signForm(b43);
+	}
+	catch (std::exception &e){
+		std::cerr << RED + "Test 3 failed: " << e.what() << RESET << std::endl;
+	}
 }
